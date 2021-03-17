6e7814ae8355334cba210fc531f5b1f2550a55b7
SiddhantRout <siddhant.rout@gmail.com>
2021-03-17 09:45:49 +0530

    Changed Folder Structure

--- src/views/index.js
+++
@@ -1,9 +0,0 @@
-import LandingPage from './LandingPage.js';
-import ViewCorrespondencePage from './ViewCorrespondencePage.js';
-import AddInvoicePage from './AddInvoicePage.js';
-
-export { 
-    LandingPage, 
-    ViewCorrespondencePage,
-    AddInvoicePage,
-};
