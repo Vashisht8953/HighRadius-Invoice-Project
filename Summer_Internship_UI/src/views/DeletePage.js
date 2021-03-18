8c1b77304a107341169e2643b1cf4192b8cf663e
SiddhantRout <siddhant.rout@gmail.com>
2021-03-18 12:06:38 +0530

    Created DeletePage

---
+++ Summer_Internship_UI/src/views/DeletePage.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { InputAdornment, makeStyles } from '@material-ui/core';
+import { Dialog, IconButton, Button, Input } from '@material-ui/core';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import { CloseIcon } from '../assets';
+
+
+const useStyles = makeStyles((theme) => ({
+    dialogPaper: {
+        minHeight: '33vh', 
+        maxHeight: '33vh',
+        minWidth: '30vw',
+        maxWidth: '30vw'
+    },
+    WindowHeader: {
+        background: '#2A3E4C',
+        borderRadius: '10px 10px 0px 0px',
+        position: 'static',
+    },
+    Title: {
+        color: '#FFFFFF',   
+        font: 'Ubuntu',
+        fontWeight: 'normal',
+    },
+    TopRightMenu: {
+        position: 'fixed',
+        top: '265px',
+        right: '570px',
+    },
+    Body: {
+        fontSize: '15px',
+        color: '#97A1A9',
+        background: '#2A3E4C',
+        // paddingBlock: '40px',
+        // border: '2px solid white',
+    },
+    CancelButton: {
+        color: '#FFF',
+        textTransform: 'none',
+        border: '1px solid #14AFF1',
+        borderRadius: 10,
+        height: '30px',
+    },
+    DeleteButton: {
+        color: '#FFF',
+        textTransform: 'none',
+        background: '#14AFF1',
+        borderRadius: '10px',
+        height: '30px',
+        paddingLeft: '15px',
+        paddingRight: '15px'
+    },
+}))
+
+const DeletePage = () => {
+    const classes = useStyles();
+    const [ open, setOpen ] = React.useState(true);
+    const [ maxWidth ] = React.useState('lg');
+    const [ fullWidth ] = React.useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    return (
+        <Dialog onClose={handleClose} open={open} maxWidth={maxWidth} fullWidth={fullWidth} classes={{ paper: classes.dialogPaper }}>
+            <MuiDialogTitle className={classes.WindowHeader}>
+                <div style={{ display: 'flex' }}>
+                    <div className={classes.Title}>
+                        Delete Record(s)?
+                    </div>
+                    <div className={classes.TopRightMenu}>
+                        <IconButton onClick={handleClose}>
+                            <CloseIcon/>
+                        </IconButton>
+                    </div>
+                </div>
+            </MuiDialogTitle>
+            <MuiDialogContent className={classes.Body}>
+                <div>
+                    <p>You'll loose your record(s) after this action. We can't recover them once deleted.</p>
+                    <p style={{ display:'flex' }}>
+                        <div>Are you sure you want to</div>
+                        <div style={{ color: '#FF5E5E', paddingLeft: '0.3rem', paddingRight: '0.3rem'  }}>permanently delete</div>
+                        <div>them?</div> 
+                    </p>
+                </div>
+            </MuiDialogContent>
+            <MuiDialogActions style={{ background: '#2A3E4C', borderRadius: '0px 0px 10px 10px' }}>
+                <div style={{ paddingRight: '10px', paddingBottom: '10px' }}>
+                    <Button autofocus onClick={handleClose} className={classes.CancelButton}>Cancel</Button>
+                </div>
+                <div style={{ paddingRight: '10px', paddingBottom: '10px' }}>
+                    <Button autofocus className={classes.DeleteButton}>Delete</Button>
+                </div>
+            </MuiDialogActions>
+        </Dialog>
+    )
+}
+
+export default DeletePage;
--- Summer_Internship_UI/src/views/index.js
+++ Summer_Internship_UI/src/views/index.js
@@ -1,9 +1,11 @@
 import LandingPage from './LandingPage.js';
 import ViewCorrespondencePage from './ViewCorrespondencePage.js';
 import AddInvoicePage from './AddInvoicePage.js';
+import DeletePage from './DeletePage.js';
 
 export { 
     LandingPage, 
     ViewCorrespondencePage,
     AddInvoicePage,
+    DeletePage,
 };
